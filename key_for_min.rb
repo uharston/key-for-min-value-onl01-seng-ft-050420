3a160a7d160b26e292b531532affaaab008e4914
Uriah Harston <uharston@gmail.com>
2020-05-07 22:08:17 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,11 +13,11 @@ def key_for_min_value(name_hash)
   #setup the necessary variables
   min_key = cheapest_item
   min_value = 0
-  
+
   #iterate
   name_hash.each do |key, value|
   if key[value] < min_value
-    cheapest_item = min_key #update this line to use the variables created    
+    cheapest_item = min_key #update this line to use the variables created
   end
   return cheapest_item #return the key
 end
@@ -26,13 +26,13 @@ def key_for_min_value(name_hash)
   #setup the necessary variables
   min_key = nil
   min_value = nil
-  
+
   #iterate
   name_hash.each do |key, value|
       if value < min_value || min_value == nil
-     min_key = key #update the value    
+     min_key = key #update the value
      min_value = value  #should maybe also uodate the key???
   end
 end
-  return key 
+  return key
 end
